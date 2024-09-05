8ac006a6da5488576f1f8712d8e73f57f1b57150
SwarnadyutiSarkar <166729648+SwarnadyutiSarkar@users.noreply.github.com>
2024-09-05 23:42:50 +0530

    read readme.md

---
+++ refugee-assistance-platform/backend/frontend/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Login from './pages/Login';
+import Home from './pages/Home';
+
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </Router>
+  );
+};
+
+export default App;
